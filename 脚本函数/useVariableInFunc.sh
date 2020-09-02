ae171a81b3ce0e47ab50f22225a366fdc652b3c7
donngchao <1559273720@qq.com>
2020-09-02 19:38:20 +0800

    update function test

--- 脚本函数/useVariableInFunc.sh
+++ 脚本函数/useVariableInFunc.sh
@@ -28,5 +28,25 @@ echo $value
 
 echo -n "Finally, try adding three or more numbers: "
 value=`addem 10 15 20`
+echo "The result of adding 10,15,20 is in this function is $value"
+
+echo -n "Adding 100 and 200:"
+value=`addem 100 200`
+echo "The result of adding 100 and 200 is: $value"
+
+function multiply {
+        if [ $# -eq 0 ] || [ $# -gt 2 ]
+        then
+                 echo -1
+        elif [ $# -eq 1 ]
+        then
+                 echo $[ $1 * $1 ]
+        else
+                 echo $[ $1 * $2 ]
+        fi
+
+}
+echo -n "multiply two numbers 2 and 3:"
+value=`multiply 2 3`
 echo $value
 
