d733afb99439e86848f3dcde4871e58066611f1f
donngchao <1559273720@qq.com>
2020-09-03 09:48:38 +0800

    use global variable in func

--- 脚本函数/globalVariable.sh
+++ 脚本函数/globalVariable.sh
@@ -10,3 +10,10 @@ read -p "Enter a value: " value
 db1
 echo "The new value is : $value"
 
+read -p "Enter a new value: " value1
+function calcSquare() {
+        result=$[ $value1 * $value1  ]
+
+}
+calcSquare
+echo "The square of $value1 is :$result"
