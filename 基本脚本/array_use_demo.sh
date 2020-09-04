3ea3d1b73096486973de06388dd77659c9cd9c6f
donngchao <1559273720@qq.com>
2020-09-04 10:59:52 +0800

    fix array unset bug

--- 基本脚本/array_use_demo.sh
+++ 基本脚本/array_use_demo.sh
@@ -3,17 +3,21 @@
 ################### 创建数组 ###################
 nums=( [ 2 ] = 2 [ 0 ] = 0 [ 1 ] = 1 )
 colors=( red yellow "dark blue" )
-
+new_arr=(a b c d)
 ################### 访问数组的单个元素 ###################
 echo ${nums[1]}
 # Output: 1
+val=${new_arr[1]}
+echo "The second element of new_arr is $val"
 
 ################### 访问数组的所有元素 ###################
 echo ${colors[*]}
 # Output: red yellow dark blue
+echo ${new_arr[*]}
 
 echo ${colors[@]}
 # Output: red yellow dark blue
+echo ${new_arr[@]}
 
 printf "+ %s\n" ${colors[*]}
 # Output:
@@ -41,15 +45,19 @@ echo ${nums[@]:0:2}
 echo ${#nums[*]}
 # Output:
 # 3
-
+echo ${#new_arr[*]}
+echo "The length of new_arr is:${#new_arr[*]}"
 ################### 向数组中添加元素 ###################
 colors=( white "${colors[@]}" green black )
 echo ${colors[@]}
 # Output:
 # white red yellow dark blue green black
-
+another_arr=( "math" "${new_arr[@]}" "alphabet" )
+echo ${another_arr[@]}
 ################### 从数组中删除元素 ###################
-unset nums[ 0 ]
+unset nums[0]
 echo ${nums[@]}
 # Output:
 # 1 2
+unset another_arr[0]
+echo ${another_arr[@]}
