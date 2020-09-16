d958e21840abdb517bfdeb072482aa4c431593d0
donngchao <1559273720@qq.com>
2020-09-16 20:34:48 +0800

    exit code

--- 基本脚本/exit_demo.sh
+++ 基本脚本/exit_demo.sh
@@ -6,4 +6,13 @@ var1=10
 var2=20
 var3=$[ $var1 + $var2 ]
 echo The answer is $var3
-exit 5
+var4=99
+var5=100
+var6=$[ $var4 * $var5 ]
+echo "The result value is $var6"
+
+exit 6
+
+#echo $? will be 6
+
+
