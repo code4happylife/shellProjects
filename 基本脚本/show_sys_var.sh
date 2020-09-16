cd5b5ff4501b0ed00ef782112c63da6c922ec714
donngchao <1559273720@qq.com>
2020-09-16 20:25:04 +0800

    sys variable

---
+++ 基本脚本/show_sys_var.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+#display user information from system
+
+echo "User info userId:$USER"
+echo "User ID: which is UID equals $UID"
+echo "Home directory is $HOME"
+#换行
+echo -e '\n'
+echo "The cost of the item is \$15"
+
