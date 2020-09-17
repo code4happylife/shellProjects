4127a88c5930f54a926386caa76be02c5b7cb257
donngchao <1559273720@qq.com>
2020-09-17 21:00:45 +0800

    at usage

---
+++ 控制/hello.sh
@@ -0,0 +1,3 @@
+#! /usr/bin/env bash
+time_now=`date`
+echo "$time_now:hello world" >> test.log
--- 控制/run_at_specific_time.sh
+++ 控制/run_at_specific_time.sh
@@ -2,4 +2,5 @@
 
 # testing the at command
 
-at -f 4.sh 22:10
+#at -f 4.sh 22:10
+at -f hello.sh 21:00
