bb7d1b991fb3f04c312091c2893ab0b4729e878f
donngchao <1559273720@qq.com>
2020-09-24 16:33:32 +0800

    select demo

--- 菜单/select_demo.sh
+++ 菜单/select_demo.sh
@@ -17,7 +17,8 @@ function menusage {
 	cat /proc/meminfo
 }
 
-PS3="Enter option:"
+#prompt info
+PS3="Enter option please:"
 select option in "Display disk space" "Display logged on users" "Display memory usage" "Exit program"
 do
 case $option in
