daedd09239dc45dfbc36fdfe9cf48fb1634691d7
donngchao <1559273720@qq.com>
2020-09-28 10:06:46 +0800

    tcl expect demo

---
+++ 系统管理/control_remote_server_exe.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/expect
+set user "root"
+set password "4G/x5nP;h`:Bk"
+set host    "127.0.0.1"
+
+#spawn will start a new process,such as ssh
+
+set timeout 5
+spawn ssh -o "StrictHostKeyChecking no" ${user}@${host}
+expect {
+"yes/no)?" { send "yes\r"; exp_continue }
+"password:" { send "${password}\r" }
+}
+
+
+expect "root*"
+send "df -h\r"
+expect "root*"
+send "cat /proc/cpuinfo\r"
+expect "root*"
+send "exit\r"
+
+
