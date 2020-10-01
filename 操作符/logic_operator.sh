aaeacefeb91f1b21cda5e3d5071a2baf7a69791d
donngchao <1559273720@qq.com>
2020-10-01 23:58:23 +0800

    logic operator test

--- 操作符/logic_operator.sh
+++ 操作符/logic_operator.sh
@@ -9,8 +9,11 @@ y=20
 if [[ -n $2 ]]; then
 	y=$2
 fi
-
-echo "x=${x}, y=${y}"
+z=40
+if [[ -n $3 ]]; then
+	z=$3
+fi
+echo "x=${x}, y=${y},z=${z}"
 
 if [[ ${x} -lt 100 && ${y} -gt 100 ]]
 then
@@ -19,12 +22,29 @@ else
 	echo "${x} -lt 100 && ${y} -gt 100 返回 false"
 fi
 
+if [[ ${z} -lt 100 && ${x} -gt 100 ]]
+then
+	echo "${z} -lt 100 && ${x} -gt 100 返回 true"
+else
+	echo "${z} -lt 100 && ${x} -gt 100 返回 false"
+fi
+
+if [[ ${z} -lt 100 || ${x} -gt 100 ]]
+then 
+	echo "${z} -lt 100 || ${x} -gt 100 return true"
+else
+	echo "${z} -lt 100 || ${x} -gt 100 return false"
+fi
+
+
 if [[ ${x} -lt 100 || ${y} -gt 100 ]]
 then
 	echo "${x} -lt 100 || ${y} -gt 100 返回 true"
 else
 	echo "${x} -lt 100 || ${y} -gt 100 返回 false"
 fi
+
+
 
 #  Output:
 #  x=10, y=20
