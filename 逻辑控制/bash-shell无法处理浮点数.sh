d91e72367acc589015a5a013b5e52e9baae0dab9
donngchao <1559273720@qq.com>
2020-10-07 10:52:54 +0800

    bash shell floating number

--- 逻辑控制/bash-shell无法处理浮点数.sh
+++ 逻辑控制/bash-shell无法处理浮点数.sh
@@ -10,3 +10,18 @@ if [ $val1 -gt 3 ]
 then
 	echo "The result is larger than 3"
 fi
+
+val2=`echo "scale=5; 3/9" |bc`
+echo "The test value is $val2"
+if [ $val2 -gt 0 ]
+then
+	echo "The result is larger than 0"
+fi
+
+<<COMMENT
+The test value is 3.3333
+./bash-shell无法处理浮点数.sh: 第 9 行:[: 3.3333: 期待整数表达式
+The test value is .33333
+./bash-shell无法处理浮点数.sh: 第 16 行:[: .33333: 期待整数表达式
+
+COMMENT
