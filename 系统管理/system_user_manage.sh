02be7d30bed85736156a427ca79d2ba1b47856df
donngchao <1559273720@qq.com>
2020-10-08 22:08:25 +0800

    user manage

--- 系统管理/system_user_manage.sh
+++ 系统管理/system_user_manage.sh
@@ -2,12 +2,15 @@
 
 ###################################################################################
 # Linux 系统用户管理
-# @author: Zhang Peng
+# @author: developer
 ###################################################################################
 
 # 创建用户组
 groupadd elk
+groupadd dctest
 # 创建新用户，-g elk 设置其用户组为 elk，-p elk 设置其密码为 elk
 useradd elk -g elk -p elk
+useradd dctest -g dctest -p dctest
 # 更改 /opt 文件夹及内部文件的所属用户及组为 elk:elk
 chown -R elk:elk /opt
+chown -R dctest:dctest ./test
