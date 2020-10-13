8446d6063e7b53deb70e440d07c5ed00fc5fa093
donngchao <1559273720@qq.com>
2020-10-13 10:33:04 +0800

    read file

--- 文件操作/readfile.sh
+++ 文件操作/readfile.sh
@@ -10,3 +10,11 @@ do
 done
 echo "Finished reading."
 
+POET=poet.txt
+line_num=1
+cat ${POET} | while read line
+do
+	echo "$line_num#$line"
+	line_num=$[ $line_num + 1 ]
+done
+echo "Finished reading poet.txt"
